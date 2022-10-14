90c6b302fd37501c8577b6430d912543f8557d36
gb <imrockbiao@gmail.com>
2022-10-14 22:53:51 +0800

    4 修改

--- server/main.c
+++ server/main.c
@@ -12,21 +12,19 @@ int main()
 
     printfLine("sfd = %d\n", sfd);
 
-    //2. 创建红黑树
+    //2. 创建根
     int epfd;
-    struct epoll_event evs[MAXONLINE];//红黑树节点发生了变化就把节点存储到这个数组里面
+    struct epoll_event evs[MAXONLINE];//根节点发生了变化就把节点存储到这个数组里面
     epfd_create(&epfd);         //创建树
     epfd_contrl(&epfd, &sfd);   //sfd上树，用于侦听客户端的连接
 
-    //3. 等待红黑树的节点发生变化
+    //3. 等待某个根节点发生变化
     while(1)
     {
         int nready =0;
         epfd_wait(&epfd, evs, &nready);
         epfd_deal(&epfd, evs, &nready, &sfd);        
     }
-
-
     printf("the Server done ...\n");
     return 0;
 }
